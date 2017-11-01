705acf3e1f1a094810c350dcdecf3d392c9abdf4
bumingxialuo <690148814@qq.com>
2017-11-01 18:13:32 +0800

    Initial Commit

---
+++ NFCDemo/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  NFCDemo
+//
+//  Created by xia on 2017/11/1.
+//  Copyright © 2017年 xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ NFCDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NFCDemo
+//
+//  Created by xia on 2017/11/1.
+//  Copyright © 2017年 xia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
