240ac15a51cc4acbe456f92851cd5252883db3e1
bumingxialuo <690148814@qq.com>
2017-11-02 10:00:06 +0800

    first commit

--- NFCDemo/ViewController.m
+++ NFCDemo/ViewController.m
@@ -7,22 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <CoreNFC/CoreNFC.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<NFCNDEFReaderSessionDelegate>
+@property (nonatomic, strong) NFCNDEFReaderSession *session;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)startBtnClick:(id)sender {
+        if ([NFCNDEFReaderSession readingAvailable]) {
+            _session = [[NFCNDEFReaderSession alloc] initWithDelegate:self queue:nil invalidateAfterFirstRead:YES];
+            
+            [_session beginSession];
+        }
+}
+
+- (void)readerSession:(NFCNDEFReaderSession *)session didDetectNDEFs:(NSArray<NFCNDEFMessage *> *)messages {
+    for (NFCNDEFMessage *message in messages) {
+        for (NFCNDEFPayload *payload in message.records) {
+            NSLog(@"Payload data:%@",payload.payload);
+        }
+    }
+}
+- (void)readerSession:(NFCNDEFReaderSession *)session didInvalidateWithError:(NSError *)error {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"错误" message:@"出现错误" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        [_session invalidateSession];
+    }];
+    [alertController addAction:cancel];
+    [self presentViewController:alertController animated:YES completion:nil];
+    NSLog(@"error: %@",error);
 }
 
 
